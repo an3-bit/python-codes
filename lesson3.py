2e6f347c2f357932ef312769c5e3ef249cfd93ab
Andrew <andrewmogambi12@gmail.com>
2024-10-29 09:46:26 +0300

    python projects

---
+++ lesson3.py
@@ -0,0 +1,10 @@
+#tuple
+# immutable -- cannot change
+
+days_of_the_week = ("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
+
+print(days_of_the_week[0])
+
+print(len(days_of_the_week))
+
+people = ("Julie",) #single values
---
+++ lesson4.py
@@ -0,0 +1,8 @@
+#dictionary
+#list [], tuple(), dictionary {}
+student = {"name": "jane sarah", "id": 1234, "age": 21, "gender": "F"}
+
+print(student["name"])
+print(student)
+
+student["weight"]=61
